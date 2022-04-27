7678576698509818093604804f17dfa543765544
RzayevGara <qara109@gmail.com>
2022-04-27 18:54:17 +0400

    cart done

--- src/components/Card/full card/Basket price/BasketPrice.js
+++ src/components/Card/full card/Basket price/BasketPrice.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-function BasketPrice() {
+function BasketPrice(props) {
   return (
     <div className="basket-price">
-        
+      <h6>Ümumi</h6>
+      <p>
+        Məbləğ
+        <span>{props.list && props.list.subtotal.formatted_with_symbol}</span>
+      </p>
+      <p>
+        Çatdırılma
+        <span>0.00</span>
+      </p>
+      <p>
+        Hədiyyə paketi
+        <span>0.00</span>
+      </p>
+      <p>
+        Promo kod
+        <span>0.00</span>
+      </p>
+      <h5>
+        Cəmi
+        <span>{props.list && props.list.subtotal.formatted_with_symbol}</span>
+      </h5>
     </div>
-  )
+  );
 }
 
-export default BasketPrice
+export default BasketPrice;
