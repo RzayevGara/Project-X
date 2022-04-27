915ff0ce72bacb59cc9b3cce808fb71eab1aaa64
RzayevGara <qara109@gmail.com>
2022-04-27 20:40:15 +0400

    cart mobile done

--- src/components/Card/full card/Basket price/BasketPrice.js
+++ src/components/Card/full card/Basket price/BasketPrice.js
@@ -3,27 +3,32 @@ import React, { useState, useEffect } from "react";
 function BasketPrice(props) {
   return (
     <div className="basket-price">
-      <h6>Ümumi</h6>
-      <p>
-        Məbləğ
-        <span>{props.list && props.list.subtotal.formatted_with_symbol}</span>
-      </p>
-      <p>
-        Çatdırılma
-        <span>0.00</span>
-      </p>
-      <p>
-        Hədiyyə paketi
-        <span>0.00</span>
-      </p>
-      <p>
-        Promo kod
-        <span>0.00</span>
-      </p>
-      <h5>
-        Cəmi
-        <span>{props.list && props.list.subtotal.formatted_with_symbol}</span>
-      </h5>
+      <div className="price-detail">
+        <h6>Ümumi</h6>
+        <p>
+          Məbləğ
+          <span>{props.list && props.list.subtotal.formatted_with_symbol}</span>
+        </p>
+        <p>
+          Çatdırılma
+          <span>0.00</span>
+        </p>
+        <p>
+          Hədiyyə paketi
+          <span>0.00</span>
+        </p>
+        <p>
+          Promo kod
+          <span>0.00</span>
+        </p>
+        <h5>
+          Cəmi
+          <span>{props.list && props.list.subtotal.formatted_with_symbol}</span>
+        </h5>
+      </div>
+      <div className="confirm-orders">
+        Sifarişləri təsdiqlə
+      </div>
     </div>
   );
 }
