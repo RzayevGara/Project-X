7cf3f77f2d9c8c46332a8d1eb56a6af030e3ed93
RzayevGara <qara109@gmail.com>
2022-04-29 16:53:47 +0400

    deleted excess imports

--- src/components/Home/productSwipe/ProductSwipe.js
+++ src/components/Home/productSwipe/ProductSwipe.js
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import Skeleton from "../../skeleton/phone/Skeleton";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
 
 const ProductSwipe = React.memo((props) => {
   return (
