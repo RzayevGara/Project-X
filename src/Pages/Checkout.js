16517d04ac0f1e412c09b7b9e22dbe142fb5e312
RzayevGara <qara109@gmail.com>
2022-05-17 17:40:06 +0400

    create address

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import "./checkout.sass";
 import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
@@ -12,30 +12,88 @@ import BasketPrice from '../components/Card/full card/Basket price/BasketPrice'
 import CheckoutUserInfo from '../components/Checkout/CheckoutUserInfo'
 import CheckoutUserAddress from '../components/Checkout/CheckoutUserAddress'
 import CheckoutPayment from '../components/Checkout/CheckoutPayment'
+import commerce from '../lib/Commerce'
+import {useDispatch, useSelector} from "react-redux";
+import {setCartID, setName, setLastName, setEmail, setPhone} from '../Reducer/CheckoutReducer'
+
 
 
 
 function Checkout() {
+
+
+
+  const dispatch = useDispatch()
+
+  const [disabled, setdisabled] = useState(true)
+
+  useEffect(() => {
+    commerce.cart.retrieve().then((cart) =>dispatch(setCartID(cart.id)));
+  })
+  const [activeStep, setActiveStep] = React.useState(0);
+
+
+  const fname = useSelector((state) => state.checkout.fname)
+  const lname = useSelector((state) => state.checkout.lname)
+  const email  = useSelector((state) => state.checkout.email)
+  const phone  = useSelector((state) => state.checkout.phone)
+  console.log(fname)
+
+  const shippingName = useSelector((state) => state.checkout.shippingName)
+  const shippingAddress = useSelector((state) => state.checkout.shippingAddress)
+  const shippingCountry  = useSelector((state) => state.checkout.shippingCountry)
+  const shippingCity  = useSelector((state) => state.checkout.shippingCity)
+
+  console.log(shippingCountry)
+
+  useEffect(() =>{
+    if(fname!=="" && lname!=="" && email!=="" && phone!==""){
+      setdisabled(false)
+    }else{
+      setdisabled(true)
+    }
+  }, [fname, email,lname,phone])
+
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  // console.log(e.target.checkValidity())
+  // dispatch(setName(e.target[0].value))
+  // dispatch(setLastName(e.target[1].value))
+  // dispatch(setEmail(e.target[2].value))
+  // dispatch(setPhone(e.target[3].value))
+}
+
+const handleSubmitSecond = (e) => {
+  e.preventDefault();
+  setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  // console.log(e.target.checkValidity())
+  // dispatch(setName(e.target[0].value))
+  // dispatch(setLastName(e.target[1].value))
+  // dispatch(setEmail(e.target[2].value))
+  // dispatch(setPhone(e.target[3].value))
+}
+
   const steps = [
     {
       label: "Şəxsi məlumatlar",
-      description: <CheckoutUserInfo/>,
+      description: <CheckoutUserInfo submit={handleSubmit}/>,
     },
     {
       label: "Çatdırılma",
-      description: <CheckoutUserAddress/>,
+      description: <CheckoutUserAddress submit={handleSubmitSecond}/>,
     },
     {
       label: "Ödəniş",
-      description: <CheckoutPayment/>,
+      description: <CheckoutPayment setActiveStep={setActiveStep}/>,
     },
   ];
 
-  const [activeStep, setActiveStep] = React.useState(0);
 
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
+};
 
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
@@ -68,7 +126,9 @@ function Checkout() {
                       <Button
                         variant="contained"
                         onClick={handleNext}
-                        sx={{ mt: 1, mr: 1 }}
+                        sx={{ mt: 1, mr: 1, }}
+                        disabled={disabled}
+
                       >
                         {index === steps.length - 1 ? "Tamamla" : "Davam"}
                       </Button>
--- src/components/Checkout/CheckoutUserInfo.js
+++ src/components/Checkout/CheckoutUserInfo.js
@@ -1,29 +1,38 @@
 import React from "react";
+import {setName, setLastName, setEmail, setPhone} from '../../Reducer/CheckoutReducer'
+import {useDispatch, useSelector} from "react-redux";
 
-function CheckoutUserInfo() {
+function CheckoutUserInfo(props) {
+  const dispatch = useDispatch()
+  const fname = useSelector((state) => state.checkout.fname)
+  const lname = useSelector((state) => state.checkout.lname)
+  const email  = useSelector((state) => state.checkout.email)
+  const phone  = useSelector((state) => state.checkout.phone)
+  
   return (
     <div className="checkout-user-info">
-      <form>
+      <form onSubmit={props.submit}>
         <div className="checkout-user-info_name_surname">
           <div>
             <label>Ad</label>
-            <input placeholder="Adınızı daxil edin" type="text" />
+            <input onChange={(e)=>{dispatch(setName(e.target.value))}} placeholder="Adınızı daxil edin" value={fname} type="text" required/>
           </div>
           <div>
             <label>Soyad</label>
-            <input placeholder="Soyadınızı daxil edin" type="text" />
+            <input onChange={(e)=>{dispatch(setLastName(e.target.value))}} placeholder="Soyadınızı daxil edin" value={lname} type="text" required/>
           </div>
         </div>
         <div className="checkout-user-info_name_surname">
           <div>
             <label>E-mail</label>
-            <input placeholder="nümunə@gmail.com" type="email" />
+            <input onChange={(e)=>{dispatch(setEmail(e.target.value))}} placeholder="nümunə@gmail.com" value={email} type="email" required/>
           </div>
           <div>
             <label>Mobil nömrə</label>
-            <input placeholder="Əlaqə nömrəsi daxil edin" type="number" />
+            <input onChange={(e)=>{dispatch(setPhone(e.target.value))}} placeholder="Əlaqə nömrəsi daxil edin" value={phone} type="number" required/>
           </div>
         </div>
+        <button style={{display: "none"}} type="submit"></button>
       </form>
     </div>
   );
